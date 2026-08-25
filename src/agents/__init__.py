f0031a5e4861b93000adc11c1dfad99d48d9c1f1
Henrique Caeiro <henrique.nascimento@cnm.org.br>
2026-08-25 11:16:30 -0300

    feat: integrate currency exchange service

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,5 +1,6 @@
 from .credit_agent import CreditAgent
 from .credit_interview_agent import CreditInterviewAgent
+from .exchange_agent import ExchangeAgent
 from .triage_agent import TriageAgent
 
-__all__ = ["CreditAgent", "CreditInterviewAgent", "TriageAgent"]
+__all__ = ["CreditAgent", "CreditInterviewAgent", "ExchangeAgent", "TriageAgent"]
